6cdf83f4b49b44283ab1f22c115ab3e7caa2c208
Pamela Ghag <pamelaghag@gmail.com>
2021-02-18 10:55:24 -0800

    google anaytics

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var projects = require('../projects.json');
 /*
  * GET home page.
  */
+ data = <some data object to pass into template>;
 
-exports.view = function(request, response){
+ exports.view = function(req, res) {
+ 	data['viewAlt'] =false;
+ 	res.render('index',data);
+ };
+ exports.viewAlt =function(req, res){
+ 	data['viewAlt']= true;
+ 	res.render('index', data);
+ };
+
+/*exports.view = function(request, response){
   	response.render('index', projects);
-};
+};*/
